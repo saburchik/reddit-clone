71dad32b4e8c9d869179fb5a6a138a2029d3dcc9
Saburov <the.saburchik@gmail.com>
2022-07-13 21:53:43 +0700

    The posts on the main page is done

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,18 @@
+import { useQuery } from '@apollo/client'
 import React from 'react'
+import { GET_ALL_POSTS } from '../graphql/queries'
+import Post from './Post'
 
 const Feed = () => {
-  return <div>Feed</div>
+  const { data, error } = useQuery(GET_ALL_POSTS)
+  const posts: Post[] = data?.getPostList
+  return (
+    <div className='mt-5 space-y-4'>
+      {posts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  )
 }
 
 export default Feed
